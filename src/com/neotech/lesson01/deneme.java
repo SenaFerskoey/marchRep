4d1a60a5cbf6ab383995ec41d982a3e610d8913c
SenaFerskoey <senacengiz121@gmail.com>
2024-03-27 14:08:03 -0400

    commit05

--- src/com/neotech/lesson01/deneme.java
+++ src/com/neotech/lesson01/deneme.java
@@ -3,6 +3,7 @@ package com.neotech.lesson01;
 public class deneme {
 	public static void main(String[] args) {
 		System.out.println("from Sena...");
+		System.out.println("and this is from brnach1...");
 	}
 
 }
